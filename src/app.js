e3213c2ef049a3ca379b9b436a12130ca76e811c
Will Smith <g.williamsmithiv@gmail.com>
2021-02-01 17:17:27 -0500

    Initial commit
    Installed express, dotenv.
    
    Need to setup a pipeline once there's something to see. Heroku?

---
+++ src/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) =>{
+    res.send(`Hello world!`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Now listening on port: ${PORT}`)
+})
