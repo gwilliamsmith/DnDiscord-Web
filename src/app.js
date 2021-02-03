576a26cd772ff0d64adb8066a6ed9e50b90b7cc3
Will Smith <g.williamsmithiv@gmail.com>
2021-02-03 12:30:43 -0500

    Install cors, react, and react-dom

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 const session = require('express-session')
@@ -12,6 +13,10 @@ db.then(() => console.log('Connected to MongoDB')).catch(err => console.log(err)
 //Routes
 const authRoute = require('./routes/auth')
 const dashboardRoute = require('./routes/dashboard')
+
+app.use(cors())
+app.use(express.json())
+
 app.use(session({
     secret : 'some random secret',
     cookie : {
@@ -21,6 +26,8 @@ app.use(session({
     name: 'D&Discord.Discord.OAuth2'
 }))
 
+
+
 //Passport
 app.use(passport.initialize())
 app.use(passport.session())
@@ -28,6 +35,10 @@ app.use(passport.session())
 //Middleware
 app.use('/auth', authRoute)
 app.use('/dashboard', dashboardRoute)
+
+app.get('/', (req, res) => {
+    res.send('Homepage')
+})
 
 app.listen(PORT, () => {
     console.log(`Now listening on port: ${PORT}`)
